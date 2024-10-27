14849c0220e635dee59487fecc96a148dea81cbf
ghada <gh.abassi01@gmail.com>
2024-10-27 23:22:56 +0100

    add-global-cors

--- springboot/app/src/main/java/com/oauth/demo/repositories/UserRepository.java
+++ springboot/app/src/main/java/com/oauth/demo/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.oauth.demo.repositories;
 
 import com.oauth.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
