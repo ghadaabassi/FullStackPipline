5a4185f027f8113431cfc1b02d90cbcfbea508e9
ghada <gh.abassi01@gmail.com>
2024-10-27 23:50:34 +0100

    add-global-cors

---
+++ springboot/app/src/main/java/com/oauth/demo/config/CORSFilter.java
@@ -0,0 +1,23 @@
+package com.oauth.demo.config;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {}
+}
