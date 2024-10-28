0e0b0f313e81e8cbaed51c5e6e515bd4135d476c
ghada <gh.abassi01@gmail.com>
2024-10-28 09:11:21 +0100

    add-global-cors

--- angular-app/src/app/services/user.service.ts
+++ angular-app/src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import { User } from '../models/User';
   providedIn: 'root',
 })
 export class UserService {
-  baseURL: string = 'http://localhost:8080/user';
+  baseURL: string = 'http://localhost:8090/user';
   constructor(private httpClient: HttpClient) {}
 
   getAllUsers(): Observable<any> {
